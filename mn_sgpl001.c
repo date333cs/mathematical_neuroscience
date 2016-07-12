b26e3f045f5c137830c470e17f8eeb194089410f
Akira Date <date@cs.miyazaki-u.ac.jp>
2016-07-12 13:29:26 +0900

    update comments

---
+++ mn_sgpl001.c
@@ -0,0 +1,23 @@
+/*
+  mn_sgpl001.c
+*/
+
+#include <stdio.h>
+#include <stdlib.h> /* for srand48(), drand48() */
+#include <math.h>   /* for sqrt(), log(), M_PI */
+#include "irl_utility.h"
+
+int main ( int argc , char * argv []){
+
+/* 
+   Experimental Procedure:
+   
+   1. make a network (initialize connection weights)
+   2. generate a random input. 
+   3. compute the winner unit
+   4. learning for all units
+   5. repeat from 2.
+
+*/
+    return 0;
+}
---
+++ mn_sgpl002.c
@@ -0,0 +1,36 @@
+/*
+  mn_sgpl002.c
+*/
+
+#include <stdio.h>
+#include <stdlib.h> /* for srand48(), drand48() */
+#include <math.h>   /* for sqrt(), log(), M_PI */
+#include "irl_utility.h"
+
+/* data structure */
+
+/*
+  
+
+
+
+
+ */
+
+
+
+
+int main ( int argc , char * argv []){
+
+/* 
+   Experimental Procedure:
+   
+   1. make a network (initialize connection weights)
+   2. generate a random input. 
+   3. compute the winner unit
+   4. learning for all units
+   5. repeat from 2.
+
+*/
+    return 0;
+}
